888fead5dacee2730478b05143d7daef5510e8c9
Alberto-Gomez-97 <120439839+Alberto-Gomez-97@users.noreply.github.com>
2023-08-15 09:46:08 -0600

    Add files via upload

---
+++ Casteos_udy/Casteos_udy/Program.cs
@@ -0,0 +1,27 @@
+﻿//CASTEOS, conversiones
+//convertir  las variables
+
+var cant1 = 7;
+var cant2 = 4;
+//casteo explicicto (porque se hace manualmente)
+var div = (double) cant1 / cant2;
+
+Console.WriteLine("La division es: " + div);
+
+//ejemplo 2
+
+byte byte1; //0 a 255,  hay que tener cuidado por el posible overflow
+int int1=3;
+
+checked{
+    byte1=(byte)int1;
+}
+Console.WriteLine(byte1);
+
+//ejemplo 3 casteo implicito
+
+byte byte2 = 11;
+int int2 ;
+//casteo implicito
+int2 = byte2;
+Console.WriteLine(int2);
